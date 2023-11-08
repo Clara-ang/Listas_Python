c5c6bdf021e3a68640bdd4640e4ce59bb15a044f
Maria Clara <claramello08@gmail.com>
2023-11-08 15:53:44 -0300

    .

---
+++ lista_7/b.py
@@ -0,0 +1,8 @@
+n = int(input())
+nn += 1
+nunu = (1 / n) + (1 / nunu) + (1 / nunu) + (1 / nunu) + (1 / n-1) + (1 / n)
+
+for i in range(1):
+    i = 1
+    num =  i / n + i / nunu + i / nunu + i / nunu + (i / n-i) + (i / n)
+    print("{:.4f}".format(num))
